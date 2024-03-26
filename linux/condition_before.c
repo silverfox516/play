3bf4fef50a36a315311f7a5508c2cf0979ecd538
igsong <igsong@tsolus.com>
2024-03-27 03:43:24 +0900

    added some example

---
+++ linux/condition_before.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+volatile int done = 0;
+
+void *child(void *arg)
+{
+    printf("child\n");
+    done = 1;
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("parent: begin\n");
+    pthread_t c;
+
+    pthread_create(&c, NULL, child, NULL);
+    while (done == 0) {
+        printf(".");
+        sleep(1);
+    }
+    printf("parent: end\n");
+    return 0;
+}
