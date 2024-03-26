b8706e4c328edc50eea1f5225c0f92e108d89b4c
igsong <igsong@tsolus.com>
2024-03-27 00:09:53 +0900

    added qt example

---
+++ qt/dialog1/src/eg_dialog.h
@@ -0,0 +1,24 @@
+#ifndef EG_DIALOG_H
+#define EG_DIALOG_H
+
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = 0);
+    ~Dialog();
+private: 
+    QLabel *label1, *label2;
+    QLineEdit *lineEdit;
+    QPushButton *button;
+private slots:
+    void showArea();
+};
+
+#endif  // EG_DIALOG_H
---
+++ qt/dialog1/src/main.cpp
@@ -0,0 +1,11 @@
+#include "eg_dialog.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Dialog w;
+    w.show();
+
+    return a.exec();
+}
